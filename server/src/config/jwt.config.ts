90d3e172fdf3e308c3b8ff97c836ac44834597c4
Jonathan Martinez <Jmartinez070589@gmail.com>
2023-01-18 06:50:23 -0600

    removed console.logs

--- server/src/config/jwt.config.ts
+++ server/src/config/jwt.config.ts
@@ -3,14 +3,6 @@ import { JwtModuleAsyncOptions } from '@nestjs/jwt';
 
 export const jwtConfig: JwtModuleAsyncOptions = {
   useFactory: (configService: ConfigService) => {
-    console.log(
-      'jwtConfig',
-      configService.get<string>('ENV_TYPE'),
-      configService.get<string>('JWT_SECRET'),
-      configService.get<string>('MONGO_URI'),
-      configService.get<string>('PORT'),
-      configService.get<string>('NODE_ENV')
-    );
     return {
       secret: configService.get<string>('SECRET_KEY'),
       signOptions: { expiresIn: 3600 },
